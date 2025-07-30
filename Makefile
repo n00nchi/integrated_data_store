d5a994ce1e2b2bd6cda3e7923b86312f5f30b0d4
n00nchi <alfredooviedo@Alfredos-MacBook-Pro.local>
2025-07-30 17:49:20 -0500

    Command line utility with getpot

--- Makefile
+++ Makefile
@@ -2,21 +2,33 @@ TARGET = bin/dbview
 SRC = $(wildcard src/*.c)
 OBJ = $(patsubst src/%.c, obj/%.o, $(SRC))
 
+# Default rule
 run: clean default
-	./$(TARGET) -f ./mynewdb.db -n 
-	./$(TARGET) -f ./mynewdb.db -a "Timmy H.,123 Sheshire Ln.,120"
+	./$(TARGET)
+#	./$(TARGET) -f ./mynewdb.db -n
+#	./$(TARGET) -f ./mynewdb.db -a "Timmy H.,123 Sheshire Ln.,120"
 
 default: $(TARGET)
 
+# Clean rule
 clean:
 	rm -f obj/*.o
 	rm -f bin/*
 	rm -f *.db
 
-$(TARGET): $(OBJ)
-	gcc -o $@ $?
+# Link target
+$(TARGET): $(OBJ) | bin
+	gcc -o $@ $^
 
-obj/%.o : src/%.c
+# Compile .c to .o, ensure obj/ exists first
+obj/%.o: src/%.c | obj
 	gcc -c $< -o $@ -Iinclude
+
+# Ensure bin and obj directories exist
+bin:
+	mkdir -p bin
+
+obj:
+	mkdir -p obj
 
 
--- src/main.c
+++ src/main.c
@@ -8,8 +8,39 @@
 #include "parse.h"
 
 void print_usage(char *argv[]) {
+  printf("Usage: %s -n -f <database file>\n", argv[0]);
+  printf("\t -n - create new database file\n");
+  printf("\t -f - (required) path to database file\n");
+  return;
 }
 
-int main(int argc, char *argv[]) { 
-	
+int main(int argc, char *argv[]) { \
+  char *filepath = NULL;
+  bool newfile = false;
+	int c;
+
+	while ((c = getopt(argc, argv, "nf:")) != -1 ) {
+	  switch (c) {
+	    case 'n':
+	              newfile = true;
+	              break;
+	    case 'f':
+	              filepath = optarg;
+	              break;
+	    case '?':
+	            printf("Unknown option -%c\nl", optopt);
+
+	            break;
+	    default:
+	            return -1;
+	  }
+	}
+      if (filepath == NULL) {
+        printf("Filepath is a required argument\n");
+        print_usage(argv);
+      }
+      printf("NewFile: %d\n", newfile);
+      printf("Filepath: %s\n", filepath);
+	    
+	    return 0;
 }
